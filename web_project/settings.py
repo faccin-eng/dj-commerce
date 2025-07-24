e093bce638222787362d852656d4ece230d9a748
Faccin-Eng <mjfaccin@hotmail.com>
2025-07-24 15:12:34 -0400

    appbar and layout inheritance

--- web_project/settings.py
+++ web_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hello',
 ]
 
 MIDDLEWARE = [
@@ -112,9 +113,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static_collected' #
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
